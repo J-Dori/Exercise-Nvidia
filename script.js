aef6e546dfc9b2db0c0d4adc6e3d5ecf7c608167
J-Dori <joelvg.stb@gmail.com>
2021-06-29 21:50:43 +0200

    Update JS to FadeOut Mobile Menu

--- script.js
+++ script.js
@@ -3,8 +3,9 @@
 const burger = document.querySelector(".burger")
 // Make a copy of Menu
 const menuMobile = document.querySelector("#menu").cloneNode(true)
-// je CHANGE l'attribut id de la copie (#menu -> #menu-mobile)
+// changing attribut id of the new copy (#menu -> #menu-mobile)
 menuMobile.setAttribute("id", "menu-mobile")
+
 
 let closeBtn = document.createElement("span")
 closeBtn.innerHTML = "&#11205;"
@@ -12,17 +13,18 @@ closeBtn.classList.add("close-btn")
 menuMobile.prepend(closeBtn)
 
 closeBtn.addEventListener("click", function(){
-    menuMobile.remove()
-    burger.style.visibility = "visible"
+    fadeOut()
 })
 
 menuMobile.addEventListener("click", function(event){
     if(event.target == this)//si l'évenement a pour cible la nav ELLE-MEME, c'est OK
-        menuMobile.remove()
+        fadeOut()
 })
 
 burger.addEventListener("click", function(){
     document.querySelector("body").prepend(menuMobile)
+    const fadeTarget = document.querySelector("#menu-mobile");
+    fadeTarget.style.opacity = 1
     burger.style.visibility = "hidden"
 })
 
@@ -31,3 +33,17 @@ window.addEventListener('resize', function(){
         menuMobile.remove()
     }
 });
+
+function fadeOut() {
+    var fadeTarget = document.getElementById("menu-mobile");
+    var fadeEffect = setInterval(function () {
+        
+        if (fadeTarget.style.opacity > 0) {
+            fadeTarget.style.opacity -= 0.05;
+        } else {
+            clearInterval(fadeEffect);
+        }
+    }, 100);
+    menuMobile.remove()
+    burger.style.visibility = "visible"
+}
